7b93e021524f4cdca54989df0acd6bbf3dab9bbf
Harshvardhan <hkakadia2003@gmail.com>
2023-07-20 17:38:22 +0530

    Day 16 Of 100Days Of commit

---
+++ Day 16/main.java
@@ -0,0 +1,19 @@
+import javax.swing.tree.TreeNode;
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        if(leftDepth==0 && rightDepth!=0) {
+            return rightDepth+1;
+        }
+        else if(leftDepth!=0 && rightDepth==0){
+            return leftDepth+1;
+        }
+        else {
+            return Math.min(leftDepth, rightDepth) + 1;
+        }
+    }
+}
