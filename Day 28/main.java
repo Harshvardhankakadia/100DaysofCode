4a20ca01a53f54125ce71ff1cecdbe6ab22aa0c4
Harshvardhan <hkakadia2003@gmail.com>
2023-08-02 19:19:59 +0530

    Day 28 Of 100Days Of Code

---
+++ Day 28/main.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int res=0; if(num==1) return false;
+        for(int i=1;i*i<=num;i++)
+            if(num%i==0)
+                res+=i+((i!=(num/i) && i!=1)?num/i:0);
+        return res==num;
+    }
+}
